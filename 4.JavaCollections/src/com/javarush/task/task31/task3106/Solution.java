399733440f1d0a5d555d66a1a0694676b8dfcb04
Abdusame <abdusame93@gmail.com>
2019-10-17 15:00:58 +0300

    add files

--- 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
@@ -1,9 +1,39 @@
 package com.javarush.task.task31.task3106;
 
-/* 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+/*
 Разархивируем файл
 */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Arrays.sort(args, 1, args.length);
+        Vector<InputStream> streams = new Vector<>();
+        for (int i = 1; i < args.length; i++) {
+            streams.add(new FileInputStream(args[i]));
+        }
+
+        SequenceInputStream sequenceInputStream = new SequenceInputStream(streams.elements());
+        ZipInputStream zis = new ZipInputStream(sequenceInputStream);
+        ZipEntry zipEntry;
+        FileOutputStream resultFile = new FileOutputStream(args[0]);
+        zipEntry = zis.getNextEntry();
+        byte[] buffer = new byte[1000];
+        int c;
+        while ((c = zis.read(buffer)) != -1) {
+            resultFile.write(buffer, 0, c);
+        }
+        zis.close();
+        resultFile.close();
     }
 }
